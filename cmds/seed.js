7987b9f5cabee45731063f1c8073b63e14672a6f
GrimZ <JooDrcrt@hotmail.fr>
2019-08-03 20:19:33 +0200

    Add cmd seed.js

---
+++ cmds/seed.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+    if (!args[0] || !args[1] || args[0 == "help"]) return message.reply("Usage: !seed @Username link titre");
+    let target = message.mentions.users.first() || message.author;
+    let link = args[1];
+    let titre = args[2];
+    message.delete().catch();
+    let embed = new Discord.RichEmbed()
+    .setTitle("**NO SEED**")
+    .addField("Lien:", link)
+    .addField("Titre:", titre)
+    .addField("User:", `${target} Merci de passer ce torrent en seed !`)
+    .setTimestamp()
+    message.channel.send({embed: embed});
+}
+
+module.exports.help = {
+    name: "seed"
+}
