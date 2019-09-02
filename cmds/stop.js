ade3d56429450c4374e05359f6e537cae0e20dfe
GrimZ <JooDrcrt@hotmail.fr>
2019-09-02 14:16:24 +0200

    Update stop.js

--- cmds/stop.js
+++ cmds/stop.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 const YTDL = require("ytdl-core");
 
-module.exports.run = async (client, message, args, ops) => {
+module.exports.run = async (bot, message, args, ops) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Tu n'as pas la permission d'exécuter cette commande!");
     if (!message.member.voiceChannel) return message.channel.send('Connecte toi a un channel vocal pour executer cette command');
     if (!message.guild.me.voiceChannel) return message.channel.send("Le bot n'est pas connecté");
