231a3274cac0847d6fffdd017f885de2fec95f5e
GrimZ <JooDrcrt@hotmail.fr>
2019-09-02 14:12:50 +0200

    Update sayinchan.js

--- cmds/sayinchan.js
+++ cmds/sayinchan.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 
-module.exports.run = async (client, msg, args) => {
+module.exports.run = async (bot, msg, args) => {
     if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("Tu n'as pas la permission d'exécuter cette commande!");
     if (!args[0] || !args[1] || args[0 == "help"]) return msg.reply("Usage: sayinchan [channelid] [message]");
-    let targetchannel = client.channels.get(args[0]);
+    let targetchannel = bot.channels.get(args[0]);
     if (!targetchannel) return msg.channel.send("Impossible de trouver le salon." + args[0]);
     let message = args.slice(1).join(' ');
     targetchannel.send(message);
