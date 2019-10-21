fbae6063dcc7d9662fd62862b3e539e41f47c621
GrimZ <JooDrcrt@hotmail.fr>
2019-10-21 09:46:40 +0200

    Add endrequest.js

---
+++ cmds/endrequest.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if (!args[0] || !args[1] || args[0 == "help"]) return message.reply("Usage: !endrequest @User Réponse");
+    let target = message.mentions.users.first() || message.author;
+    let requestrep = args.join(" ");
+    message.delete().catch();
+    let embed = new Discord.RichEmbed()
+    .setTitle("**END Request**")
+    .addField("Réponse de la requete:", requestrep)
+    .addField("User:", `${target} Voici la réponse de ta request !`)
+    .setTimestamp()
+    target.send({embed: embed});
+    message.author.send(`${message.author} La réponse a la requête a bien était envoyé a ${target}`)
+}
+
+module.exports.help = {
+    name: "endrequest"
+}
