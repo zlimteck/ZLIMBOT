37bec36a47b987eae0f2466dd8aeb222fde2458f
GrimZ <JooDrcrt@hotmail.fr>
2019-10-21 09:45:59 +0200

    Add request.js

---
+++ cmds/request.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if (!args[0] || !args[1] || args[0 == "help"]) return message.reply("Usage: !request Ta Requête");
+    let request = args.join(" ");
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag, message.author.displayAvatarURL)
+    .setColor("#D50A0A")
+    .setTitle("**Requête**")
+    .addField("Du membre", `${message.author}`)
+    .addField("Requête", request)
+    .addField("Date", message.createdAt);
+    var requestchannel = message.guild.channels.find(requestchannel => requestchannel.name === "requête")
+    if (!requestchannel) return message.channel.send("Impossible de trouver le salon request.");
+    message.channel.send(`${message.author} Ta requête a bien été prise en compte, tu recevras un MP quand celle-ci sera complétée !`);
+    requestchannel.send(embed);
+    message.delete().catch();
+}
+
+module.exports.help = {
+    name: "request"
+}
