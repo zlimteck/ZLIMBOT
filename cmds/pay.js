274e189c6fcd9a1db518783a095c8e3e01e6733a
GrimZ <JooDrcrt@hotmail.fr>
2019-11-02 16:18:48 +0100

    Update pay.js

--- cmds/pay.js
+++ cmds/pay.js
@@ -10,6 +10,10 @@ module.exports.run = async (bot, message, args) => {
 
     let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   
+    if (pUser.id === message.author.id){
+      return message.reply("Arrete de vouloir tricher !!")
+    }
+
     if(!coins[pUser.id]){
       coins[pUser.id] = {
         coins: 0
