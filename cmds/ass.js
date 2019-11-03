d2b6c592ac4d0fcae93e15048347178a3e4025ff
GrimZ <JooDrcrt@hotmail.fr>
2019-11-03 19:55:04 +0100

    Add ass.js

---
+++ cmds/ass.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const {get} = require("snekfetch");
+
+module.exports.run = async (bot, message, args) => {
+    if (!message.channel.nsfw) return message.channel.send(":underage: Tu dois etre dans un channel NSFW pour exécuter cette commande !");
+    const {body} = await get("https://nekobot.xyz/api/image?type=ass")
+    let embed = new Discord.RichEmbed()
+    .setTitle("Ass")
+    .setColor("#E642AA")
+    .setDescription("Voici une photo d'un cul mon cochon...")
+    .setImage(body.message)
+    .setFooter(`Demandé par ${message.author.username}`)
+    .setTimestamp();
+    message.channel.send({embed: embed});
+    message.delete().catch();
+}
+
+module.exports.help = {
+    name: "ass"
+}
