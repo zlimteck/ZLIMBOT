06b6f61dff34bec66820a2d2b3f6779bde621342
GrimZ <JooDrcrt@hotmail.fr>
2019-11-07 17:54:35 +0100

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const moment = require("moment")
+let success = require("../successusers.json");
+let xp = require("../xp.json");
 const status = {
     online: "Online",
     idle: "Idle",
@@ -7,10 +9,27 @@ const status = {
     offline: "Offline/Invisible"
 };
 
-module.exports.run = async (bot, message, args) => {
-    let target = message.mentions.users.first() || message.author;
+module.exports.run = async (bot, message, args, con) => {
+    let target = message.mentions.users.first() || message.author || message.author.id;
     let guild = message.channel.guild;
     let role = guild.roles.find(role => role.name)
+
+    if(!xp[target.id]){
+        xp[target.id] = {
+          xp: 0,
+          level: 1
+       };
+     }
+
+    if(!success[target.id]){
+        success[target.id] = {
+          success: "Tu n'as encore obtenu aucun trophy, dommage .."
+        };
+    }
+
+    let trophy = success[target.id].success;
+    let curxp = xp[target.id].xp;
+    let curlvl = xp[target.id].level;
     let embed = new Discord.RichEmbed()
     .setAuthor("Infos User")
     .setThumbnail(target.displayAvatarURL)
@@ -19,6 +38,9 @@ module.exports.run = async (bot, message, args) => {
     .addField("❯ Discriminator", `#${target.discriminator}`, true)
     .addField("❯ ID", target.id)
     .addField("❯ Role(s)", `${message.guild.member(target).roles.map(roles => `\`${roles.name}\``).slice(0).join(", ")}`)
+    .addField('❯ Trophy obtenu', trophy)
+    .addField('❯ Level', curlvl, true)
+    .addField('❯ Xp', curxp, true)
     .addField("❯ Status", `${status[target.presence.status]}`, true)
     .addField("❯ Joue a", target.presence.game, true)
     .addField("❯ Dernier Message", target.lastMessage)
@@ -26,7 +48,7 @@ module.exports.run = async (bot, message, args) => {
     .setFooter(`Demandé par ${message.author.username}`)
     .setTimestamp()
     message.channel.send({embed: embed});
-	message.delete().catch();
+    message.delete().catch();
 }
 
 module.exports.help = {
