85108708b5852a21fec8bb7d61704e8ecab7abd0
GrimZ <JooDrcrt@hotmail.fr>
2020-02-12 12:55:36 +0100

    Update hot.js

--- cmds/hot.js
+++ cmds/hot.js
@@ -1,27 +1,29 @@
 const Discord = require("discord.js");
-const randomPuppy = require('random-puppy');
-const request = require('snekfetch');
+const snekfetch = require('snekfetch')
 
 module.exports.run = async (bot, message, args) => {
-    var subreddits = [
-        "gonewild",
-    ]
-    var sub = subreddits[Math.floor(Math.random() * subreddits.length)];
-    randomPuppy(sub)
-    .then(url => {
+    try {
+        const { body } = await snekfetch
+        .get('https://www.reddit.com/r/gonewild.json?sort=top&t=week')
+        .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return message.channel.send('Il semble que je suis à court de nouvelle photos hot ! Réessaye plus tard.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
         let embed = new Discord.RichEmbed()
-        .setTitle("Hot")
         .setColor("#F7BA2A")
-        .setDescription("Voici une photo HOT")
-        .setImage(url)
-        .setTimestamp()
+        .setTitle("Photo fourni par r/gonewild")
+        .setImage(allowed[randomnumber].data.url)
+        .setDescription("Posté par: " + allowed[randomnumber].data.author)
+        .addField("Info:", "Up votes: " + allowed[randomnumber].data.ups + " / Commentaires: " + allowed[randomnumber].data.num_comments)
         .setFooter(`Demandé par ${message.author.username}`)
-        .setTimestamp();
+        .setTimestamp()
         message.channel.send({embed: embed});
         message.delete().catch();
-    })
+    } catch (err) {
+        return console.log(err);
+    }
 }
 
 module.exports.help = {
     name: "hot"
-}   
+}      
