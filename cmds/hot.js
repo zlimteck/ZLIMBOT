f0eb69f33970b0b35b0c86be98407392a00ea0e2
GrimZ <JooDrcrt@hotmail.fr>
2020-02-13 20:31:54 +0100

    Update hot.js

--- cmds/hot.js
+++ cmds/hot.js
@@ -6,15 +6,16 @@ module.exports.run = async (bot, message, args) => {
         const { body } = await snekfetch
         .get('https://www.reddit.com/r/gonewild.json?sort=top&t=week')
         .query({ limit: 800 });
+        if (!message.channel.nsfw) return message.channel.send(":underage: Tu dois etre dans un channel NSFW pour exécuter cette commande !");
         const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
         if (!allowed.length) return message.channel.send('Il semble que je suis à court de nouvelle photos hot ! Réessaye plus tard.');
         const randomnumber = Math.floor(Math.random() * allowed.length)
         let embed = new Discord.RichEmbed()
-        .setColor("#F7BA2A")
-        .setTitle("Photo fourni par r/gonewild")
+        .setColor("#E642AA")
+        .setTitle("Subreddit r/gonewild")
         .setImage(allowed[randomnumber].data.url)
         .setDescription("Posté par: " + allowed[randomnumber].data.author)
-        .addField("Info:", "Up votes: " + allowed[randomnumber].data.ups + " / Commentaires: " + allowed[randomnumber].data.num_comments)
+        .setURL(allowed[randomnumber].data.url)
         .setFooter(`Demandé par ${message.author.username}`)
         .setTimestamp()
         message.channel.send({embed: embed});
