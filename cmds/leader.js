0d806d87d5d0ee10eb579c8edae7626282370d07
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 21:21:45 +0100

    Update leader.js
    Adding log information

--- cmds/leader.js
+++ cmds/leader.js
@@ -20,6 +20,10 @@ module.exports.run = async (client, message, args) => {
         .setTimestamp()
         message.channel.send(embed);
         message.delete().catch();
+
+        console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+        console.log(`Tableau des leaders Level demandé par ${message.author.username}`)
+
        }
 
        if(args[0] === 'ecoin') {
@@ -38,6 +42,9 @@ module.exports.run = async (client, message, args) => {
     .setTimestamp()
     message.channel.send(embed);
     message.delete().catch();
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    console.log(`Tableau des leaders Ecoin demandé par ${message.author.username}`)
 
 }
 
