4fb97233d436c493e1aa446fcc81f10bd0c211ff
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 20:13:58 +0100

    Update emojis.js
    Adding log information

--- cmds/emojis.js
+++ cmds/emojis.js
@@ -10,6 +10,9 @@ module.exports.run = async (bot, message, args) => {
     .setTimestamp()
     message.channel.send({embed: embed});
     message.delete().catch();
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    
 }
 
 module.exports.help = {
