f86ce519b8b01943d083cc319bfbbf6320c7aeb1
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 18:37:16 +0100

    Update ban.js
    Adding log information

--- cmds/ban.js
+++ cmds/ban.js
@@ -11,8 +11,8 @@ module.exports.run = async (bot, message, args) => {
     let banUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if (!banUser) return errors.cantfindUser(message.channel);
     if (banUser.id === bot.user.id) return errors.botuser(message);
-    let banReason = args.join(" ").slice(22);
-    if (!banReason) return errors.noReason(message.channel);
+    let banRaison = args.join(" ").slice(22);
+    if (!banRaison) return errors.noReason(message.channel);
     if (banUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, banUser, "MANAGE_MESSAGES");
     let banEmbed = new Discord.RichEmbed()
     .setDescription("Ban")
@@ -21,11 +21,15 @@ module.exports.run = async (bot, message, args) => {
     .addField("Ban par", `<@${message.author.id}> ID ${message.author.id}`)
     .addField("Ban depuis le channel", message.channel)
     .addField("Date", message.createdAt)
-    .addField("Raison(s)", banReason);
+    .addField("Raison(s)", banRaison);
     var banchannel = message.guild.channels.find(banchannel => banchannel.name === "ban")
     if (!banchannel) return message.channel.send("Impossible de trouver le salon ban.")
-    message.guild.member(banUser).ban(banReason);
+    message.guild.member(banUser).ban(banRaison);
     banchannel.send(banEmbed);
+    message.delete().catch();
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    console.log(`Le membre ${banUser} a etait bannie du serveur ${message.guild.name} pour la raison suivante: ${banRaison} ! le ${message.createdAt}`)
 }
 
 module.exports.help = {
