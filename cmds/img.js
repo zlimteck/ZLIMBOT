cbb290646c53729fa6713091077eaf3fa4d829f4
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 21:04:33 +0100

    Update img.js
    Adding log information

--- cmds/img.js
+++ cmds/img.js
@@ -8,10 +8,14 @@ module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setTitle("❯ Image:")
     .setColor("#D50A0A")
+    .setURL(args.join(" "))
     .setImage(args.join(" "))
     .setFooter(`Image envoyée par ${message.author.username}`)
     .setTimestamp()
     message.channel.send({embed: embed});
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    console.log(`Image envoyé par ${message.author.username}: ${args.join(" ")}`)
 }
 
 module.exports.help = {
