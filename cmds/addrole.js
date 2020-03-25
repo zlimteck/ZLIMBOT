b8894c1768b05e36f8fec91972742e19b5055e51
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 17:43:05 +0100

    Update addrole.js
    Adding log information

--- cmds/addrole.js
+++ cmds/addrole.js
@@ -27,6 +27,10 @@ module.exports.run = async (bot, message, args, lang) => {
     logschannel.send(logembed);
     message.delete().catch();
     user.send(`Félicitations tu obtiens le rôle ${foundRole.name}`)
+
+     console.log(`Ajout du role ${foundRole.name} a ${user} effectué par ${message.author.username} le ${message.createdAt}`)
+     console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+
 };
 
 module.exports.help = {
