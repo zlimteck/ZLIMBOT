e75c0180c8404c60ef15d1191380db339fe7216e
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 22:51:32 +0100

    Update mp.js
    Adding log information

--- cmds/mp.js
+++ cmds/mp.js
@@ -5,11 +5,14 @@ module.exports.run = async (bot, message, args) => {
     let target = message.mentions.users.first() || message.author;
     let mpUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
     let mpMessage = args.join(" ").slice(22);
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Tu n'as pas la permission d'exécuter cette commande!");
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Tu n'as pas la permission d'exécuter cette commande!");
     let botmessage = args.join(" ");
     message.delete().catch();
     mpUser.send(`${mpUser} ${mpMessage}`)
     message.author.send(`${message.author} Tu as envoyé un message privé a ${mpUser}`)
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    
 }
 
 module.exports.help = {
