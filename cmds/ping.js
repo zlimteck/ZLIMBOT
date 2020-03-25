0e43003cbe49dd6dcffa653251a1691393f992c2
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 23:19:01 +0100

    Update ping.js
    Adding log information

--- cmds/ping.js
+++ cmds/ping.js
@@ -6,6 +6,9 @@ module.exports.run = async (bot, message) => {
     .addField(`❯ Ping ${message.author.username}`, (new Date().getTime() - message.createdTimestamp + " ms"))
     message.channel.send({embed: embed});
     message.delete().catch();
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    
 }
 
 module.exports.help = {
