d5b08f493c1c0e9f3ab51dd77c8ac34ac2ab6a36
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 19:58:12 +0100

    Update createrole.js
    Adding log information and update perms

--- cmds/createrole.js
+++ cmds/createrole.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR")) {
-        message.reply("Tu n'as pas la permission de créer un role")
-    }else{
+    if (!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
         let createrole = args.join(" ")
         message.guild.createRole({
             name: createrole,
@@ -12,8 +10,11 @@ module.exports.run = async (bot, message, args) => {
         })
         
         message.channel.send(`Le role ${createrole} a bien été crée !`)
+        message.delete().catch();
+
+        console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+        console.log(`Le role ${args.join(" ")} a été crée sur le serveur ${message.guild.name} par ${message.author.username} le ${message.createdAt}`)
     }
-}
 
 module.exports.help = {
     name: "createrole"
