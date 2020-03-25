f931f98ef5588094fd9d1b89e5b6413f083258e9
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 17:50:10 +0100

    Update afk.js
    Adding log information

--- cmds/afk.js
+++ cmds/afk.js
@@ -20,6 +20,8 @@ module.exports.run = async (bot, message, args) => {
         let botmessage = `${user} **N'EST PLUS AFK !**`
         message.channel.send(botmessage);
     },ms(time))
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
 }
 
 module.exports.help = {
