7c49f26fa2a27e3aaaaed6367bf1724758dd2cb4
GrimZ <JooDrcrt@hotmail.fr>
2020-03-25 20:14:56 +0100

    Update endrequest.js
    Adding log information

--- cmds/endrequest.js
+++ cmds/endrequest.js
@@ -12,6 +12,9 @@ module.exports.run = async (bot, message, args) => {
     .setTimestamp()
     target.send({embed: embed});
     message.author.send(`${message.author} La réponse a la requête a bien était envoyé a ${target}`)
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    
 }
 
 module.exports.help = {
