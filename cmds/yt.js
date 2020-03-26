c97af06df4d8a4b150c70428bb910c41a31a5652
GrimZ <JooDrcrt@hotmail.fr>
2020-03-26 16:55:07 +0100

    Update yt.js
    Adding log information

--- cmds/yt.js
+++ cmds/yt.js
@@ -20,7 +20,10 @@ module.exports.run = async (bot, message, args, ops) => {
     .setFooter(`Lecture lancé par ${message.author.username}`)
     .setTimestamp()
     message.channel.send({embed: embed});
-	message.delete().catch();
+    message.delete().catch();
+    
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    
 }
 
 module.exports.help = {
