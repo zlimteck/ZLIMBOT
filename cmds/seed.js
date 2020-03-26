bcb54013a2aba1095df97572c5c3f66f92b3ac2f
GrimZ <JooDrcrt@hotmail.fr>
2020-03-26 14:53:27 +0100

    Update seed.js
    Adding log information

--- cmds/seed.js
+++ cmds/seed.js
@@ -7,14 +7,20 @@ module.exports.run = async (bot, message, args) => {
     let link = args[1];
     let titre = args[2];
     message.delete().catch();
+    var releaseschannel = message.guild.channels.find(releaseschannel => releaseschannel.name === "releases");
+    if (!releaseschannel) return message.channel.send("Impossible de trouver le salon releases.");
     let embed = new Discord.RichEmbed()
     .setTitle("**NO SEED**")
     .addField("Lien:", link)
     .addField("Titre:", titre)
     .addField("User:", `${target} Merci de passer ce torrent en seed !`)
     .setTimestamp()
+    releaseschannel.send(embed);
     target.send({embed: embed});
     message.author.send(`${message.author} La demande de mise en seed a bien était envoyé a ${target}`)
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    
 }
 
 module.exports.help = {
