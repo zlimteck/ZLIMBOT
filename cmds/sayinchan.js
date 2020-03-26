88301b5302675fd85140e6f86aad7f182b9bdd44
GrimZ <JooDrcrt@hotmail.fr>
2020-03-26 14:52:57 +0100

    Update sayinchan.js
    Adding log information

--- cmds/sayinchan.js
+++ cmds/sayinchan.js
@@ -1,14 +1,18 @@
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 
-module.exports.run = async (bot, msg, args) => {
-    if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("Tu n'as pas la permission d'exécuter cette commande!");
-    if (!args[0] || !args[1] || args[0 == "help"]) return msg.reply("Usage: sayinchan [channelid] [message]");
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Tu n'as pas la permission d'exécuter cette commande!");
+    if (!args[0] || !args[1] || args[0 == "help"]) return message.reply("Usage: sayinchan [channelid] [message]");
     let targetchannel = bot.channels.get(args[0]);
-    if (!targetchannel) return msg.channel.send("Impossible de trouver le salon." + args[0]);
-    let message = args.slice(1).join(' ');
-    targetchannel.send(message);
-    msg.delete().catch();
+    if (!targetchannel) return message.channel.send("Impossible de trouver le salon." + args[0]);
+    let botmessage = args.slice(1).join(' ');
+    targetchannel.send(botmessage);
+    message.delete().catch();
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    console.log(`Message envoyer via le bot par ${message.author.username} dans le salon ${args[0]}`)
+
 }
 
 module.exports.help = {
