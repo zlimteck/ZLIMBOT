1edcc6e37b27fa549789b95e4c8d4741e0908295
GrimZ <JooDrcrt@hotmail.fr>
2020-03-26 15:00:04 +0100

    Update space.js
    Adding log information

--- cmds/space.js
+++ cmds/space.js
@@ -22,6 +22,9 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send({embed: embed});
         message.delete().catch();
     })
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    
 }
 
 module.exports.help = {
