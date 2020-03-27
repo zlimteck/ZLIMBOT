7b360558e81af20ae4e19e64d5a48619dbc74d03
GrimZ <JooDrcrt@hotmail.fr>
2020-03-27 14:05:51 +0100

    Update stop.js
    Adding log information

--- radios/stop.js
+++ radios/stop.js
@@ -16,6 +16,7 @@ module.exports.run = async (bot, message, args, ops) => {
     message.delete().catch();
 
     console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    console.log(`${bot.user.username} a quitté un salon vocal`)
 
 }
 
