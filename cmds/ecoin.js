bfca7c684d9c655902399c16760d9a21055fdcb9
GrimZ <JooDrcrt@hotmail.fr>
2020-03-30 17:51:07 +0200

    Update ecoin.js
    Add target

--- cmds/ecoin.js
+++ cmds/ecoin.js
@@ -1,28 +1,28 @@
 const Discord = require("discord.js");
 let coins = require("../ecoin.json");
 
-module.exports.run = async (bot, message, args) => {
-    let target = message.mentions.users.first() || message.guild.members.get(args[1]) || message.author.username;
+module.exports.run = async (bot, message) => {
+  let target = message.mentions.users.first() || message.author || message.author.id;
 
-    if(!coins[message.author.id]){
-      coins[message.author.id] = {
+    if(!coins[target.id]){
+      coins[target.id] = {
         coins: 0
       };
     }
   
-    let Ecoin = coins[message.author.id].coins;
+    let Ecoin = coins[target.id].coins;
         let embed = new Discord.RichEmbed()
         .setColor("#15f153")
-        .setAuthor(message.author.tag, message.author.displayAvatarURL)
+        .setAuthor(target.tag, target.displayAvatarURL)
         .setThumbnail("https://imgur.com/yARdHmJ.jpg")
         .addField('Solde restant:', `${Ecoin} Ecoin`)
-        .setFooter(`Solde de ${message.author.username} affiché via E Corp !`)
+        .setFooter(`Solde de ${target.username} affiché via E Corp !`)
         .setTimestamp()
         message.channel.send(embed);
         message.delete().catch();
 
         console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
-        console.log(`Solde ecoin de ${message.author.username}: ${Ecoin} ecoin`)
+        console.log(`Solde ecoin de ${target.username}: ${Ecoin} ecoin`)
 
 }
 
