a2b0be2368168ef70d239c7caa3513cc5821b930
ZlimTeck <JooDrcrt@hotmail.fr>
2020-05-20 00:19:16 +0200

    Update pickaxe.js
    rewrite

--- cmds/pickaxe.js
+++ cmds/pickaxe.js
@@ -17,57 +17,55 @@ module.exports.run = async (bot, message, args) => {
         pick[message.author.id] = {
             pick: 1
         };
-      }
+    }
 
-      if (!coins[message.author.id]){
+    if (!coins[message.author.id]){
         return message.reply("Tu n'as pas d'Ecoin dans ton portefeuille !")
-      }
+    }
 
-      if(sCoins < "50000") return message.reply("Tu n'as pas assez d'Ecoin pour effectuer cette achat !");
+    if(sCoins < "50000") return message.reply("Tu n'as pas assez d'Ecoin pour effectuer cette achat !");
 
-      coins[message.author.id] = {
+    coins[message.author.id] = {
         coins: pCoins - parseInt("50000")
-      };
-      fs.writeFile("./ecoin.json", JSON.stringify(coins), (err) => {
+    };
+    fs.writeFile("./ecoin.json", JSON.stringify(coins), (err) => {
         if(err) cosole.log(err)
-      });
+    });
 
-      coins[guild.owner.id] = {
+    coins[guild.owner.id] = {
         coins: dealer + parseInt("50000")
-      };
-      fs.writeFile("./ecoin.json", JSON.stringify(coins), (err) => {
+    };
+    fs.writeFile("./ecoin.json", JSON.stringify(coins), (err) => {
         if(err) cosole.log(err)
-      });
+    });
 
-      pick[message.author.id] = {
+    pick[message.author.id] = {
         pick: pPick + parseInt(1)
-      };
-      fs.writeFile("./pickaxe.json", JSON.stringify(pick), (err) => {
+    };
+    fs.writeFile("./pickaxe.json", JSON.stringify(pick), (err) => {
         if(err) cosole.log(err)
-      });
+    });
 
-      let embed = new Discord.RichEmbed()
-      .setTitle("Achat réussi")
-      .setAuthor(message.author.tag, message.author.displayAvatarURL)
-      .addField("Achat:", "Pickaxe", true)
-      .addField("Prix:", `50 000 Ecoin`, true) 
-      .setTimestamp()
-      message.channel.send(embed);
-      message.delete().catch();
+    let embed = new Discord.RichEmbed()
+    .setTitle("Achat réussi")
+    .setAuthor(message.author.tag, message.author.displayAvatarURL)
+    .addField("Achat:", "Pickaxe", true)
+    .addField("Prix:", `50 000 Ecoin`, true) 
+    .setTimestamp()
+    message.channel.send(embed);
+    message.delete().catch();
 
-      let dealerembed = new Discord.RichEmbed()
-      .setColor("#615755")
-      .setTitle("**Récupération des ecoin suite a l'achat d'une pioche**")
-      .addField("Achat:", `50 000 Ecoin suite a l'achat de ${message.author.username}`)
-      .setFooter(`Paiement`, bicon);
-      guild.owner.send({embed: dealerembed});
+    let dealerembed = new Discord.RichEmbed()
+    .setColor("#615755")
+    .setTitle("**Récupération des ecoin suite a l'achat d'une pioche**")
+    .addField("Achat:", `50 000 Ecoin suite a l'achat de ${message.author.username}`)
+    .setFooter(`Paiement`, bicon);
+    guild.owner.send({embed: dealerembed});
 
-      console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
-      console.log(`Ajout d'une pioche dans l'inventaire de ${message.author.id} le ${message.createdAt}`)
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    console.log(`Ajout d'une pioche dans l'inventaire de ${message.author.id} le ${message.createdAt}`)
+}
 
-    }
-
-    module.exports.help = {
-        name:"pickaxe"
-    }
-
+module.exports.help = {
+    name:"pickaxe"
+}
