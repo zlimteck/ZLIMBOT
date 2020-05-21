90c6332d9af80c739c4eae2abb71f93c089f476d
ZlimTeck <JooDrcrt@hotmail.fr>
2020-05-21 22:51:58 +0200

    Update xp.js
    Add target function, and rewrite

--- cmds/xp.js
+++ cmds/xp.js
@@ -4,29 +4,31 @@ let xp = require("../xp.json");
 module.exports.run = async (bot, message, args) => {
     let target = message.mentions.users.first() || message.guild.members.get(args[1]) || message.author;
 
-    if(!xp[message.author.id]){
-        xp[message.author.id] = {
-          xp: 0,
-          level: 1
-       };
-     }
-        let curxp = xp[message.author.id].xp;
-        let curlvl = xp[message.author.id].level;
-        let nxtLvlXp = curlvl * 500;
-        let difference = nxtLvlXp - curxp;
-        let embed = new Discord.RichEmbed()
-        .setColor("#15f153")
-        .setThumbnail(message.author.displayAvatarURL)
-        .addField("Membre", `${message.author}`)
-        .addField('Level', curlvl, true)
-        .addField('XP', curxp, true)
-        .setFooter(`${message.author.username} il te faut encore ${difference} pour level up !`)
-        .setTimestamp()
-        message.channel.send(embed);
-        message.delete().catch();
+    if(!xp[target.id]){
+        xp[target.id] = {
+            xp:0,
+            level: 1
+        };
+    }
 
-      console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
-      console.log(`${message.author} Level: ${curlvl} XP: ${curxp} Prochain level dans ${difference} xp`)
+    let curxp = xp[target.id].xp;
+    let curlvl = xp[target.id].level;
+    let nxtLvlXp = curlvl * 500;
+    let difference = nxtLvlXp - curxp;
+    let xpembed = new Discord.RichEmbed()
+    .setColor("#15f153")
+    .setThumbnail(target.displayAvatarURL)
+    .addField("Membre", `${target}`)
+    .addField('Level', curlvl, true)
+    .addField('XP', curxp, true)
+    .setFooter(`${target.username} Level up dans ${difference} xp!`)
+    .setTimestamp()
+    message.channel.send(xpembed);
+    message.delete().catch();
+
+    console.log(`Commande ${message.author.lastMessage} executé sur le serveur ${message.guild.name} dans le salon ${message.channel.name} par le membre ${message.author.username} le ${message.createdAt}`)
+    console.log(`${target} Level: ${curlvl} XP: ${curxp} Prochain level dans ${difference} xp`)
+
 }
 
 module.exports.help = {
